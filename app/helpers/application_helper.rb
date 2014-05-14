31ef2a5c8d2c8f957c6e7c7da84551a7b71b60a8
Hc <82017876@qq.com>
2014-05-14 18:04:11 +0800

    new_hose update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,15 @@ module ApplicationHelper
     end
   end
   
+  def full_key(page_key)
+    base_key = "漳州房产网,漳州房产,漳州房地产,漳州买房,漳州新房,漳州房价,漳州楼盘,角美房产网,漳州开发区房产网"
+    if page_key.empty?
+      base_key
+    else
+      page_key
+    end
+  end
+  
   MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
                         'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
                         'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
