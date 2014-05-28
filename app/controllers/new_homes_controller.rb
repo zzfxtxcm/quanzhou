8574e08679c111aec2c6b65902b45fdf924a3be4
Hc <82017876@qq.com>
2014-05-28 09:14:53 +0800

    new house alter

--- app/controllers/new_homes_controller.rb
+++ app/controllers/new_homes_controller.rb
@@ -10,6 +10,7 @@ class NewHomesController < ApplicationController
     @keyword = Sunspot.search(NewHome) do
       keywords params[:keyword]
       with(:area_id).equal_to(params[:area_id]) if params[:area_id].present?
+      with(:status_id).equal_to(params[:status_id]) if params[:status_id].present?
       with(:section_id).equal_to(params[:section_id]) if params[:section_id].present?
       with(:area_range_id).equal_to(params[:area_range_id]) if params[:area_range_id].present?
       with(:property_type_id).equal_to(params[:property_type_id]) if params[:property_type_id].present?
@@ -58,6 +59,6 @@ class NewHomesController < ApplicationController
                    .per_page(9)
     end
 
-    add_breadcrumb "正文", new_home_path    
+    add_breadcrumb "正文", new_home_path
   end
 end
